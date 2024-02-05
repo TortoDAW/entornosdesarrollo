9fbe65bc8f51ec19cb4e743478033bf64d6576e9
TortoDAW <2622461@alu.murciaeduca.es>
2024-02-05 17:05:47 +0100

    Agregado metodo y actualizado README

--- Principal.java
+++ Principal.java
@@ -1,5 +1,13 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Principal {
     public static void main(String[] args){
         System.out.println("Hola Mundo");
+
+        String dateTime = DateTimeFormatter.ofPattern("MMM dd yyyy, hh:mm:ss a")
+        .format(LocalDateTime.now());
+
+        System.out.println(dateTime);
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Practica 4.1 Git inicial
+## git checkout -b Practica4_1 para moverme de rama
